800712b4e36c8611661af2db2995eac61dbc70fc
Clemo97 <lumumbaclement@gmail.com>
2022-05-16 16:00:08 +0300

    correcting email

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -4,7 +4,7 @@ import unittest
 
 class CommentModelTest(unittest.TestCase):
     def setUp(self):
-        self.user_Peris = User(username = 'Peris',password = 'mango', email = 'oduolpepe618@gmail.com')
+        self.user_Peris = User(username = 'Peris',password = 'mango', email = 'lumumbaclement@gmail.com')
         self.new_blog = Blog(id=1,title_blog='Test',blog_content='This is a Blog test',category="interview",user = self.user_Peris)
         self.new_comment = Comment(id=1,comment='Test comment',user=self.user_Peris,blog=self.new_blog)
 
